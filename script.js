787fb0fa8110af1dd152380bb5858c597165d8cf
Jahnavi <venkata.venkamsetty@students.iiit.ac.in>
2024-03-29 22:52:17 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,22 @@
+document
+  .getElementById("visualization-btn")
+  .addEventListener("click", function () {
+    window.location.href = "chloropeth.html";
+  });
+
+document.getElementById("code-link-btn").addEventListener("click", function () {
+  window.location.href = "https://example.com/code";
+});
+
+document
+  .getElementById("video-link-btn")
+  .addEventListener("click", function () {
+    window.location.href = "https://example.com/video";
+  });
+
+document
+  .getElementById("report-link-btn")
+  .addEventListener("click", function () {
+    window.location.href =
+      "https://iiitaphyd-my.sharepoint.com/:w:/g/personal/renu_sree_students_iiit_ac_in/EW64tEOS1wtMjmrNE7OShaUBG-GJQVtgghh8Wo1Gt4UslA?rtime=I3e44c5P3Eg";
+  });
